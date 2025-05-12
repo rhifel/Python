daae585c47eb6ba9c4bff5efeb792079e05ddf58
Rhifel <rhifelmarbuscado@gmail.com>
2025-05-12 17:17:51 +0800

    Problem 2

--- challenges.py
+++ challenges.py
@@ -29,3 +29,42 @@ elif n % 2 == 0 and 6 <= n <= 20:
     print("Weird")
 else:
     print("Not Weird")
+
+'''
+Task
+The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+Example
+a = 3
+b = 5
+Print the following:
+
+8
+-2
+15
+Input Format
+
+The first line contains the first integer, .
+The second line contains the second integer, .
+
+Constraints
+1 <= a <= 10^10
+1 <= b <= 10^10
+
+Output Format
+
+Print the three lines as explained above.
+'''
+#Problem 2
+a = int(input())
+b = int(input())
+sums = a + b
+diff = a - b
+product = a * b
+    
+print(sums)
+print(diff)
+print(product)
