13b8de2286ddc0ecb24f3e16b75d3c10e282f8d3
Rhifel <rhifelmarbuscado@gmail.com>
2025-05-13 19:13:48 +0800

    HackeRanK

--- challenges.py
+++ challenges.py
@@ -117,3 +117,29 @@ Note that "" represents the consecutive values in between.
 n = int(input())
 for i in range(1, n+1):
     print(i, end="")
+
+'''
+You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+Hello firstname lastname! You just delved into python.
+Function Description
+Complete the print_full_name function in the editor below.
+print_full_name has the following parameters:
+string first: the first name
+string last: the last name
+'''
+#Problem 7
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
+
+def print_full_name(first, last):
+    # Write your code here
+    print(f"Hello {first} {last}! You just delved into python.")
+
+if __name__ == '__main__':
+    print_full_name()
+    
