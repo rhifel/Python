705c9c03034e8784fb35c15b3ee8424bb5d983e9
Rhifel <rhifelmarbuscado@gmail.com>
2025-05-13 19:13:48 +0800

    HackeRanK

--- challenges.py
+++ challenges.py
@@ -33,7 +33,6 @@ else:
 '''
 Task
 The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
-
 The first line contains the sum of the two numbers.
 The second line contains the difference of the two numbers (first - second).
 The third line contains the product of the two numbers.
@@ -41,21 +40,16 @@ Example
 a = 3
 b = 5
 Print the following:
-
 8
 -2
 15
 Input Format
-
 The first line contains the first integer, .
 The second line contains the second integer, .
-
 Constraints
 1 <= a <= 10^10
 1 <= b <= 10^10
-
 Output Format
-
 Print the three lines as explained above.
 '''
 #Problem 2
