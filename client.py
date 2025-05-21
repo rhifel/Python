f1e2ef694aaad74ff6d2ba037848760157160f75
rhifel <161362805+rhifel@users.noreply.github.com>
2025-05-21 22:52:27 +0800

    practicing sockets

---
+++ client.py
@@ -0,0 +1,25 @@
+import socket 
+
+HEADER = 64
+PORT = 4090
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECTED!"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_lenght = len(message)
+    send_lenght = str(msg_lenght).encode(FORMAT)
+    send_lenght += b' ' * (HEADER - len(send_lenght))
+    client.send(send_lenght)
+    client.send(message)
+
+send("Hello There")
+input()
+send(DISCONNECT_MESSAGE)
+send("exit")
+input()
