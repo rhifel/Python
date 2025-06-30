20b21a976e4d3536c9f0e7f7f74a3c3652ac4fda
Rhifel <rhifelmarbuscado@gmail.com>
2025-07-01 01:22:34 +0800

    HackerRank and Random Python Challenges/Questions

--- challenges.py
+++ challenges.py
@@ -1,132 +1,134 @@
-#Hackerrank Challenges
-'''
-Task
-Given an integer, , perform the following conditional actions:
+# Hackerrank Challenges / Random Questions I came across
 
-If n is odd, print Weird
-If n is even and in the inclusive range of  to , print Not Weird
-If n is even and in the inclusive range of  to , print Weird
-If n is even and greater than , print Not Weird
-Input Format
+# Task
+# Given an integer, , perform the following conditional actions:
 
-A single line containing a positive integer, .
+# If n is odd, print Weird
+# If n is even and in the inclusive range of  to , print Not Weird
+# If n is even and in the inclusive range of  to , print Weird
+# If n is even and greater than , print Not Weird
+# Input Format
 
-Constraints
-1 <= n <= 100
-Output Format
+# A single line containing a positive integer, .
 
-Print Weird if the number is weird. Otherwise, print Not Weird.
+# Constraints
+# 1 <= n <= 100
+# Output Format
 
-'''
+# Print Weird if the number is weird. Otherwise, print Not Weird.
 
 #Problem 1
-n = int(input().strip())
-if not n % 2 == 0:
-    print("Weird")
-elif n % 2 == 0 and 2 <= n <= 5: 
-    print("Not Weird")
-elif n % 2 == 0 and 6 <= n <= 20:
-    print("Weird")
-else:
-    print("Not Weird")
+# n = int(input().strip())
+# if not n % 2 == 0:
+#     print("Weird")
+# elif n % 2 == 0 and 2 <= n <= 5: 
+#     print("Not Weird")
+# elif n % 2 == 0 and 6 <= n <= 20:
+#     print("Weird")
+# else:
+#     print("Not Weird")
 
-'''
-Task
-The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
-The first line contains the sum of the two numbers.
-The second line contains the difference of the two numbers (first - second).
-The third line contains the product of the two numbers.
-Example
-a = 3
-b = 5
-Print the following:
-8
--2
-15
-Input Format
-The first line contains the first integer, .
-The second line contains the second integer, .
-Constraints
-1 <= a <= 10^10
-1 <= b <= 10^10
-Output Format
-Print the three lines as explained above.
-'''
+
+# Task
+# The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+# Example
+# a = 3
+# b = 5
+# Print the following:
+# 8
+# -2
+# 15
+# Input Format
+# The first line contains the first integer, .
+# The second line contains the second integer, .
+# Constraints
+# 1 <= a <= 10^10
+# 1 <= b <= 10^10
+# Output Format
+# Print the three lines as explained above.
+
 #Problem 2
-a = int(input())
-b = int(input())
-sums = a + b
-diff = a - b
-product = a * b
+# a = int(input())
+# b = int(input())
+# sums = a + b
+# diff = a - b
+# product = a * b
     
-print(sums)
-print(diff)
-print(product)
+# print(sums)
+# print(diff)
+# print(product)
 
-'''
-Add logic to print two lines. The first line should contain the result of integer division, a//b. 
-The second line should contain the result of float division, a / b.
 
-No rounding or formatting is necessary.
-'''
+# Add logic to print two lines. The first line should contain the result of integer division, a//b. 
+# The second line should contain the result of float division, a / b.
+
+# No rounding or formatting is necessary.
+
+
 #Problem 3
-a = int(input())
-b = int(input())
-div = a / b
-div1 = a // b
-print(div1) 
-print(div)
+# a = int(input())
+# b = int(input())
+# div = a / b
+# div1 = a // b
+# print(div1) 
+# print(div)
 
-'''
-Task
-The provided code stub reads an integer, , from STDIN. 
-For all non-negative integers , print i^2 .
-Example
-n = 3
-The list of non-negative integers that are less than n = 3 is [0,1,2]. 
-Print the square of each number on a separate line.
-'''
+
+# Task
+# The provided code stub reads an integer, , from STDIN. 
+# For all non-negative integers , print i^2 .
+# Example
+# n = 3
+# The list of non-negative integers that are less than n = 3 is [0,1,2]. 
+# Print the square of each number on a separate line.
+
 #Problem 4
-n = int(input())
-for i in range(n):
-    print(i * i)
+# n = int(input())
+# for i in range(n):
+#     print(i * i)
 
-'''
-Given a year, determine whether it is a leap year. 
-If it is a leap year, return the Boolean True, otherwise return False.
-Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. 
-It is only necessary to complete the is_leap function.
-'''
+
+# Given a year, determine whether it is a leap year. 
+# If it is a leap year, return the Boolean True, otherwise return False.
+# Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. 
+# It is only necessary to complete the is_leap function.
+
+
 #Problem 5
-def is_leap(year):
-    leap = False
-    if year % 4 == 0:
-         if year % 100 != 0 or year % 400 == 0 :
-            leap = True
-    return leap
+# def is_leap(year):
+#     leap = False
+#     if year % 4 == 0:
+#          if year % 100 != 0 or year % 400 == 0 :
+#             leap = True
+#     return leap
 
-year = int(input())
-print(is_leap(year))
+# year = int(input())
+# print(is_leap(year))
 
-'''
-The included code stub will read an integer, , from STDIN.
-Without using any string methods, try to print the following:
-Note that "" represents the consecutive values in between.
-'''
+
+# The included code stub will read an integer, , from STDIN.
+# Without using any string methods, try to print the following:
+# Note that "" represents the consecutive values in between.
+
+
 #Problem 6
-n = int(input())
-for i in range(1, n+1):
-    print(i, end="")
+# n = int(input())
+# for i in range(1, n+1):
+#     print(i, end="")
 
-'''
-You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
-Hello firstname lastname! You just delved into python.
-Function Description
-Complete the print_full_name function in the editor below.
-print_full_name has the following parameters:
-string first: the first name
-string last: the last name
-'''
+
+# You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+# Hello firstname lastname! You just delved into python.
+# Function Description
+# Complete the print_full_name function in the editor below.
+# print_full_name has the following parameters:
+# string first: the first name
+# string last: the last name
+
+
 #Problem 7
 # Complete the 'print_full_name' function below.
 #
@@ -136,40 +138,62 @@ string last: the last name
 #  2. STRING last
 #
 
-def print_full_name(first, last):
-    # Write your code here
-    print(f"Hello {first} {last}! You just delved into python.")
+# def print_full_name(first, last):
+#     # Write your code here
+#     print(f"Hello {first} {last}! You just delved into python.")
 
-if __name__ == '__main__':
-    print_full_name()
-'''
-Task
-You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+# if __name__ == '__main__':
+#     print_full_name()
 
-Function Description
 
-Complete the split_and_join function in the editor below.
+# Task
+# You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
 
-split_and_join has the following parameters:
+# Function Description
 
-string line: a string of space-separated words
-Returns:
-string: the resulting string
-'''
-def split_and_join(line):
-    # write your code here
+# Complete the split_and_join function in the editor below.
+
+# split_and_join has the following parameters:
+
+# string line: a string of space-separated words
+# Returns:
+# string: the resulting string
+
+
+# def split_and_join(line):
+#     write your code here
     
-    a = line.split(" ")
-    #print(a)
+#     a = line.split(" ")
+#     print(a)
     
-    b = "-".join(a)
-    #print(b)
+#     b = "-".join(a)
+#     print(b)
     
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    #split_and_join("this is a string")
-    line = input()
-    result = split_and_join(line)
-    print()
-    
-    
+#     split_and_join("this is a string")
+#     line = input()
+#     result = split_and_join(line)
+#     print()
+
+
+# Came across this problem the code is given but the question is what will be the output 
+# its easy right obviously it will be [] [] [] but the default list continue to grow internally
+# [1]
+# []
+# [1, 2]
+# []
+# [1, 2, 3]
+# []
+# lesson ? I guess we need to be careful of usign mutable arguments like lists and dictionaries 
+# unless you have a reason on using them
+
+def mutate(x, y = []):
+    y.append(x)
+    print(y) # i added this to print the default lists
+    y = []
+    return y
+
+print(mutate(1))
+print(mutate(2))
+print(mutate(3))
