a15eee4611f450a7d3e242e4399478001e4c85e6
Rhifel <rhifelmarbuscado@gmail.com>
2025-07-05 14:16:11 +0800

    added a func for full path so that i can compile the whole game

--- running_game/running_game.py
+++ running_game/running_game.py
@@ -2,23 +2,32 @@ import pygame
 from sys import exit
 from time import sleep
 from random import randint, choice
+import sys
+import os
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS  # For PyInstaller temp path
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 # Player Class
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        player_walk_1 = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
-        player_walk_2 = pygame.image.load("graphics/Player/player_walk_2.png").convert_alpha()
+        player_walk_1 = pygame.image.load(resource_path("graphics/Player/player_walk_1.png")).convert_alpha()
+        player_walk_2 = pygame.image.load(resource_path("graphics/Player/player_walk_2.png")).convert_alpha()
 
         self.player_walk = [player_walk_1, player_walk_2]
         self.player_index = 0
-        self.player_jump = pygame.image.load('graphics/Player/jump.png').convert_alpha()
+        self.player_jump = pygame.image.load(resource_path('graphics/Player/jump.png')).convert_alpha()
         
         self.image = self.player_walk[self.player_index]
         self.rect = self.image.get_rect(midbottom = (80, 300))
         self.grav = 0
         
-        self.jump_sound = pygame.mixer.Sound('audio/jump.mp3')
+        self.jump_sound = pygame.mixer.Sound(resource_path('audio/jump.mp3'))
         self.jump_sound.set_volume(0.5)
         
     def player_input(self):
@@ -59,14 +68,14 @@ class Obstacle(pygame.sprite.Sprite):
         super().__init__()
         
         if type == 'fly':
-            fly_img_1 = pygame.image.load("graphics/Fly/Fly1.png").convert_alpha()
-            fly_img_2 = pygame.image.load("graphics/Fly/Fly2.png").convert_alpha()
+            fly_img_1 = pygame.image.load(resource_path("graphics/Fly/Fly1.png")).convert_alpha()
+            fly_img_2 = pygame.image.load(resource_path("graphics/Fly/Fly2.png")).convert_alpha()
             self.frames = [fly_img_1, fly_img_2]
             y_pos = 210
             
         else:
-            snail_img_1 = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
-            snail_img_2 = pygame.image.load("graphics/snail/snail2.png").convert_alpha()
+            snail_img_1 = pygame.image.load(resource_path("graphics/snail/snail1.png")).convert_alpha()
+            snail_img_2 = pygame.image.load(resource_path("graphics/snail/snail2.png")).convert_alpha()
             self.frames = [snail_img_1, snail_img_2]
             y_pos = 300
         
@@ -126,14 +135,14 @@ player.add(Player())
 obstacle_group = pygame.sprite.Group()
 
 # Font Style
-font_style = pygame.font.Font("fonts/Pixeltype.ttf", 50)
+font_style = pygame.font.Font(resource_path("fonts/Pixeltype.ttf"), 50)
 
 # Background Images
-sky_img = pygame.image.load("graphics/Sky.png").convert()
-ground_img = pygame.image.load("graphics/ground.png").convert()
+sky_img = pygame.image.load(resource_path("graphics/Sky.png")).convert()
+ground_img = pygame.image.load(resource_path("graphics/ground.png")).convert()
 
 # Background Music
-bg_music = pygame.mixer.Sound('audio/music.wav')
+bg_music = pygame.mixer.Sound(resource_path('audio/music.wav'))
 bg_music.set_volume(0.4)
 bg_music.play(loops= -1)
 
@@ -144,7 +153,7 @@ quit_rect = quit_txt.get_rect(center=(400, 350))
 over_txt = font_style.render("Press Space to Play", False, "#085C5C")
 over_rect = over_txt.get_rect(center=(400, 50))
 
-player_stand = pygame.image.load("graphics/Player/player_stand.png").convert_alpha()
+player_stand = pygame.image.load(resource_path("graphics/Player/player_stand.png")).convert_alpha()
 player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
 player_stand_rect = player_stand.get_rect(center=(400, 200))
 
